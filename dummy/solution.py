b417f8da2cafac632bdf4b9dcaf7de57c5ae00ca
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2020-12-17 17:50:05 +0100

    This is way more usefule

--- dummy/solution.py
+++ dummy/solution.py
@@ -1,8 +1,22 @@
+def part1(puzzle):
+    return -1
+
+
+def part2(puzzle):
+    return -1
+
+
 def main():
     with open('input') as f:
-        puzzle = f.read()
-    print('Solution Part 1:', )
-    print('Solution Part 2:',)
+        puzzle = f.readlines()
+    with open('example') as f:
+        example = f.readlines()
+
+    assert part1(example) == 0
+    print('Solution Part 1:', part2(puzzle))
+
+    assert part2(example) == 0
+    print('Solution Part 2:', part2(puzzle))
 
 
 if __name__ == '__main__':
